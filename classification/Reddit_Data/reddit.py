643da349054d4a2c9ffaccb256623591c6e7ba09
Gwyneth Pearson <gpearson@wustl.edu>
2016-06-06 13:22:05 -0500

    updated reddit script

--- classification/Reddit_Data/reddit.py
+++ classification/Reddit_Data/reddit.py
@@ -6,46 +6,73 @@ import os
 # subreddits may have liberals in them but they are most likely not going to be a large factor in most of the lengthy rants on Reddit
 
 #gets 25*times comments from a certain subreddit_title
-def getrequest(subreddit_title,folder_name = "",last_comment_name = None, times=10):
+def getrequest(folder_name,con_last_comment_name = None, lib_last_comment_name = None):
     print "TIMES = " + str(times)
     if times <= 0:
         print "End of Data Collection"
         return
     
-
-    if last_comment_name == None:
-        r = requests.get("https://reddit.com/r/{0}/comments.json?".format(subreddit_title), headers={'User-agent':'Comment Bot 0.1'})
+    #r/conservative
+    if con_last_comment_name == None:
+        r_con = requests.get("https://reddit.com/r/Conservative/comments.json?", headers={'User-agent':'Comment Bot 0.1'})
         #r.text
 
     else:
-        r = requests.get("https://reddit.com/r/{0}/comments.json?".format(subreddit_title) + "after="+last_comment_name, headers={'User-agent':'Comment Bot 0.1'})
+        r_con = requests.get("https://reddit.com/r/Conservative/comments.json?" + "after="+con_last_comment_name, headers={'User-agent':'Comment Bot 0.1'})
     
 
-    request_json = r.json()
-    comments = request_json['data']['children']
+    con_request_json = r_con.json()
+    con_comments = con_request_json['data']['children']
+
+    count = 0    
+    con_comment = ""
+    con_comment_name = ""
+    for q in con_comments:
+        c_score = q['data']['score'].encode("ascii","ignore").decode("ascii")
+        con_comment = q['data']['body'].encode("ascii","ignore").decode("ascii")
+        con_comment_name = q['data']['name'].encode("ascii","ignore").decode("ascii")
+        if (len(con_comment) > 100 and c_score > 0):
+            count += 1
+            file_name = folder_name + "/train/con/" + con_comment_name + ".txt"
+            #save comment
+            if count % 5 == 0:
+                file_name = folder_name + "/test/" + con_comment_name + "_Conservative.txt"
+            target = open(file_name, "a+")
+            target.write(con_comment)
+            target.write("\n")
+            target.close()
+
+    #r/liberal
+     if lib_last_comment_name == None:
+        r_lib = requests.get("https://reddit.com/r/Liberal/comments.json?", headers={'User-agent':'Comment Bot 0.1'})
+        #r.text
+
+    else:
+        r_lib = requests.get("https://reddit.com/r/Liberal/comments.json?" + "after="+lib_last_comment_name, headers={'User-agent':'Comment Bot 0.1'})
+    
+
+    lib_request_json = r_lib.json()
+    lib_comments = lib_request_json['data']['children']
 
 
     count = 0    
-    comment = ""
-    comment_name = ""
-    for q in comments:
-        comment = q['data']['body'].encode("ascii","ignore").decode("ascii")
-        comment_name = q['data']['name'].encode("ascii","ignore").decode("ascii")
-        if (len(comment) > 100):
+    l_comment = ""
+    l_comment_name = ""
+    for q in lib_comments:
+        l_score = q['data']['score'].encode("ascii","ignore").decode("ascii")
+        l_comment = q['data']['body'].encode("ascii","ignore").decode("ascii")
+        l_comment_name = q['data']['name'].encode("ascii","ignore").decode("ascii")
+        if (len(l_comment) > 100 and l_score > 0 ):
             count += 1
-            file_name= ""
-            if (subreddit_title=="Conservative"):
-                file_name = folder_name + "/train/con/" + comment_name + ".txt"
-            else:
-                file_name = folder_name + "/train/lib/" + comment_name + ".txt"
+            file_name = folder_name + "/train/lib/" + l_comment_name + ".txt"
             #save comment
             if count % 5 == 0:
-                file_name = folder_name + "/test/" + comment_name + "_" + subreddit_title + ".txt"
+                file_name = folder_name + "/test/" + l_comment_name + "_Liberal.txt"
             target = open(file_name, "a+")
-            target.write(comment)
+            target.write(l_comment)
             target.write("\n")
             target.close()
-    getrequest(subreddit_title, folder_name,comment_name, times-1)
+    getrequest(folder_name,con_comment_name, l_comment_name)
 
 def setup():
     folder_name = sys.argv[1]
@@ -60,8 +87,7 @@ def setup():
     os.makedirs(folder_name + "/train/con")
     os.makedirs(folder_name + "/train/lib")
     os.makedirs(folder_name + "/test")
-    getrequest("Conservative",folder_name)
-    getrequest("Liberal",folder_name)
+    getrequest(folder_name)
 
 setup()
 
