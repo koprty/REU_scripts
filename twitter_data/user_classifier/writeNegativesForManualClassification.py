80e324047b59ce014aa7b47d6f9969a583294dc5
koprty <koprty@gmail.com>
2016-07-01 17:43:38 -0500

    added helper script for writing users in need of manual classification

---
+++ twitter_data/user_classifier/writeNegativesForManualClassification.py
@@ -0,0 +1,103 @@
+import sqlite3
+
+
+
+'''
+	#UPDATE USERS alREADY CLASSIFIED from users and tweets9_users (without connection code)
+	o=0
+	query = "select Usr_ID, Screename, Description, Category from users where Usr_ID in (select Usr_ID from tweets9_musers);"
+	query = "select Usr_ID, Screename, Description, Category from tweets9_users where Usr_ID in (select Usr_ID from tweets9_musers);"
+	cursor.execute(query)
+	data = cursor.fetchall()
+	
+	for x in data: 
+		query = "UPDATE tweets9_musers set Screename = '%s', Description = \"\"\"%s\"\"\", category = '%s' where Usr_ID = '%s'"%(x[1], x[2], x[3], x[0])
+		print query
+		cursor.execute(query)
+		conn.commit()
+		o+=1
+	print o
+	exit()
+
+'''
+def writeToFile():
+	conn = sqlite3.connect("../tweets.sqlite")
+	cursor  = conn.cursor()
+
+	query = "select Usr_ID, Screename, Description from tweets9_users where category = 'null';"
+	query = "select Usr_ID, Screename, Description from tweets9_musers where category = 'null';"
+	cursor.execute(query)
+	
+
+	results = cursor.fetchall()
+	s = ""
+	#re = results[:len(results)/3]
+	
+	for r in results:
+		x = 0
+		while x < len(r):
+			if x ==1:
+				s+= "\t"
+			s += str(r[x]).replace("\r", "\t").replace("\n", "\t")
+			s+= "\t"
+			x+=1
+		s += "\n"
+
+	conn.close()
+
+
+	f = open("data.txt", "wb")
+	f.write(s)
+	f.close()
+
+
+writeToFile()
+print "writing done"
+
+# Usr_ID is trimmed off due to xlsx format -__-
+# using screenames to relate
+def importCategoriesDB(fname):
+	f = open (fname, "r")
+	content = f.read()
+	#print content
+	f.close()
+
+	conn = sqlite3.connect("../tweets.sqlite")
+	cursor = conn.cursor()
+	lines = content.split("\r")
+	#print lines
+	i = 0
+	for line in lines:
+
+		l = line.split("\t")
+		if len(l) != 4:
+			print l
+			exit()
+		cate = l[1]
+		sn = l[2]
+		
+		query = "UPDATE tweets9_users set category = '%s' where Screename = '%s'"%(cate, sn)
+
+		cursor.execute(query)
+		print query 
+		i+=1
+		conn.commit()
+	conn.close()
+
+	print i
+	return content.split("\r")
+
+
+#x =importCategoriesDB ("partA.txt")
+#print len(x)
+
+#x =importCategoriesDB ("dataA.txt")
+#print len(x)
+
+#print x
+
+
+
+
+
+
