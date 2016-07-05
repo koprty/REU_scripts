1bf182feb1932e66be30b8beffbf9368bfabbe47
Gwyneth Pearson <gpearson@wustl.edu>
2016-07-05 13:16:21 -0500

    script for finding weights in classifier

---
+++ twitter_classifying/weights.py
@@ -0,0 +1,38 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import SVC
+from scipy.sparse import lil_matrix
+import cPickle
+
+
+transformer = cPickle.load(open('new_tf2.pickle','rb'))
+SVC = cPickle.load(open('SVC2.pickle','rb'))
+vectorizer = cPickle.load(open('new_vect2.pickle','rb'))
+
+coeff_i =  SVC.coef_.indices
+coeff_d = SVC.coef_.data
+vocab =  vectorizer.vocabulary_
+
+#index = vocab['high schoolers']
+'''
+i=0
+coeff = []
+for c in coeff_i:
+	if abs(coeff_d[i])>.4
+		tup = (coeff_i[i],coeff_d[i])
+		coeff.append(tup)
+	i+=1
+
+
+for c in coeff: 
+'''
+for v in vocab:
+	if "high" in v or "oil" in v:
+		index = vocab[v]
+		i=0
+		for c in coeff_i:
+			if c == index:
+				if abs(coeff_d[i])>.15:
+					print v + " "+ str(coeff_d[i])
+			i +=1
+
+
