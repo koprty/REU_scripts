1a80f47a25d727000c28e5c6e7fa928aaf7b7072
Gwyneth Pearson <gpearson@wustl.edu>
2016-07-05 16:28:27 -0500

    topic modeling function added as well as query for insertion

--- classify_model.py
+++ classify_model.py
@@ -1,7 +1,19 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+
 import cPickle
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.svm import SVC
+
+import gensim
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, mod
+
+conn = sqlite3.connect("tweets.sqlite")
+cursor = conn.cursor()
 
 def preprocess():
 	pass
@@ -18,8 +30,22 @@ def classify_mdab(tweet = []):
 
 	return result[0][1]> 0.830430012805
 
-def run_topic_model():
-	pass
+def run_topic_model(tweet = ""):
+	tokenizer = RegexpTokenizer(r'\w+')
+	p_stemmer = PorterStemmer()
+	en_stop = list(set(get_stop_words('en')) | set(stopwords.words('english'))) + ['u','oh', 'uh', 'im', 'n', 'dont', 'ur']
+
+	raw = tweet.lower()
+	tokens =tokenizer.tokenize(raw)
+	stopped_tokens = [j for j in tokens if not j in stopwords.words('english')]
+	stemmed_tokens = [p_stemmer.stem(j) for j in stopped_tokens]
+
+	d = corpora.Dictionary.load("topic_modeling/twitter/tweet_dict.dict")
+	bow = d.doc2bow(stemmed_tokens)
+
+	lda = models.ldamodel.LdaModel.load("topic_modeling/twitter/lda_9.ginsem")
+	tweet_lda = lda.get_document_topics(bow,minimum_probability = .001)
+	return tweet_lda
 
 def classify_user():
 	pass
@@ -28,11 +54,33 @@ def get_followers_following():
 	pass
 
 def classify_and_model():
+	#assuming we've pulled Tweet_ID, Usr_ID, Screename, CreatedAt, hashtags, and Tweet_Text
 	#preprocess
 	#classify as positive or negative (MAKE SURE TWEET IS IN LIST, e.g. ["this is the tweet"])
 	if classify_mdab([tweet_text]):
 		#run topic_model
+		tweet_topic_dist = run_topic_model(tweet_txt)
+		top_topic = sorted(tweet_topic_dist,key=lambda x: x[1], reverse = True)[0]
+		spaced_topics = ""
+		for top in tweet_topic_dist:
+			space_topics += top[1]
+			if (top[0]!=8):
+				space_topics += " "
 		#update database with new tweet
+		query = ("insert into TABLE_NAME "
+				 "(Tweet_ID, Usr_ID, Screename, hashtags, Tweet_Text, CreatedAt, DateChecked, "
+				 "TopTopic, SpaceTopic, Zero, One, Two, Three, Four, Five, Six, Seven, Eight) "
+				 "values (" + tweet_id + ", " + usr_id + ", '" + screename + "', '" + hashtags + "',"
+				 " '" + tweet_text + "', '" + creationDate + "', '" + datetime.datetime.now() + "',"
+				 " " + top_topic[0] + ", '" + space_topics + "', ")
+		for top in tweet_topic _dist:
+			query += top[1]
+			if (top[0]!=8):
+				query += ", "
+			else:
+				query += ");"
+		cursor.execute()
+		conn.commit()
 
 		#see if user already in previous databases
 			#if not, run user classification
